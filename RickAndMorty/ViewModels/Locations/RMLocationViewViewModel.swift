eba53efa1a38aeba52b41d21f9275c6b57e02654
Carson Gross <carsongro@gmail.com>
2023-08-22 18:36:25 -0600

    Finished location pagination and updated character collection view for larger devices

--- RickAndMorty/ViewModels/Locations/RMLocationViewViewModel.swift
+++ RickAndMorty/ViewModels/Locations/RMLocationViewViewModel.swift
@@ -26,15 +26,30 @@ final class RMLocationViewViewModel {
         }
     }
     
+    public var isLoadingMoreLocations = false
+    public var shouldShowLoadMoreIndicator: Bool {
+        apiInfo?.next != nil
+    }
+    
+    private var didFinishPagination: (() -> Void)?
+    
     // Location response info
     // Will contain next url if present
     private var apiInfo: RMGetAllLocationsResponse.Info?
     
     public private(set) var cellViewModels = [RMLocationTableViewCellViewModel]()
     
+    private var hasMoreResults: Bool {
+        false
+    }
+    
+    // MARK: Init
+    
     init() {
         
     }
+    
+    // MARK: Public
     
     public func location(at index: Int) -> RMLocation? {
         guard index < locations.count && index >= 0 else {
@@ -42,6 +57,46 @@ final class RMLocationViewViewModel {
         }
         
         return locations[index]
+    }
+    
+    public func registerDidFinishPaginationBlock(_ block: @escaping () -> Void) {
+        self.didFinishPagination = block
+    }
+    
+    /// Paginate if additional locations are needed
+    public func fetchAdditionalLocations() {
+        guard let nextURLString = apiInfo?.next,
+              let url = URL(string: nextURLString),
+              !isLoadingMoreLocations else {
+            return
+        }
+        
+        isLoadingMoreLocations = true
+        
+        guard let request = RMRequest(url: url) else {
+            isLoadingMoreLocations = false
+            return
+        }
+        
+        RMService.shared.execute(request, expecting: RMGetAllLocationsResponse.self) { [weak self] result in
+            guard let self else { return }
+            
+            switch result {
+            case .success(let responseModel):
+                let moreResults = responseModel.results
+                let info = responseModel.info
+                self.apiInfo = info
+                self.locations.append(contentsOf: moreResults)
+                DispatchQueue.main.async {
+                    // Notify via callback
+                    self.didFinishPagination?()
+                    self.isLoadingMoreLocations = false
+                }
+            case .failure(let failure):
+                print(String(describing: failure))
+                self.isLoadingMoreLocations = false
+            }
+        }
     }
     
     public func fetchLocations() {
@@ -60,9 +115,5 @@ final class RMLocationViewViewModel {
                 break
             }
         }
-    }
-    
-    private var hasMoreResults: Bool {
-        false
     }
 }
--- RickAndMorty/Views/Location/RMLocationView.swift
+++ RickAndMorty/Views/Location/RMLocationView.swift
@@ -23,6 +23,13 @@ final class RMLocationView: UIView {
             UIView.animate(withDuration: 0.3) {
                 self.tableView.alpha = 1
             }
+            
+            self.viewModel?.registerDidFinishPaginationBlock { [weak self] in
+                DispatchQueue.main.async {
+                    self?.tableView.tableFooterView = nil // No more loading indicator data
+                    self?.tableView.reloadData()
+                }
+            }
         }
     }
     
@@ -81,6 +88,10 @@ final class RMLocationView: UIView {
     public func configure(with viewModel: RMLocationViewViewModel) {
         self.viewModel = viewModel
     }
+    
+    public func reloadData() {
+        self.tableView.reloadData()
+    }
 }
 
 // MARK: Table View Delegate
@@ -117,5 +128,41 @@ extension RMLocationView: UITableViewDataSource {
         let cellViewModel = cellViewModels[indexPath.row]
         cell.configure(with: cellViewModel)
         return cell
+    }
+}
+
+extension RMLocationView: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard let viewModel = viewModel,
+              !viewModel.cellViewModels.isEmpty,
+              viewModel.shouldShowLoadMoreIndicator,
+              !viewModel.isLoadingMoreLocations else {
+            return
+        }
+        Timer.scheduledTimer(withTimeInterval: 0.2, repeats: false) { [weak self] t in
+            let offset = scrollView.contentOffset.y
+            let totalContentHeight = scrollView.contentSize.height
+            let totalScrollViewFixedHeight = scrollView.frame.size.height
+            
+            if offset >= (totalContentHeight - totalScrollViewFixedHeight - 120) {
+                DispatchQueue.main.async {
+                    self?.showLoadingIndicator()
+                }
+                viewModel.fetchAdditionalLocations()
+            }
+            t.invalidate()
+        }
+    }
+    
+    private func showLoadingIndicator() {
+        let footer = RMTableLoadingFooterView(
+            frame: CGRect(
+                x: 0,
+                y: 0,
+                width: frame.width,
+                height: 100
+            )
+        )
+        tableView.tableFooterView = footer
     }
 }
